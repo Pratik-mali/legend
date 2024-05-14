63eb176eef8a0d4b34e7ec5762316235ef799305
Pratik <pratikm057@gmail.com>
2024-05-14 21:48:12 +0530

    my repo

--- project/src/App.js
+++ project/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://legenderfx.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          legenderfx.com
         </a>
       </header>
     </div>
