c8115b89c19c4472c2b73863ba35411a91f84654
Pratik <pratikm057@gmail.com>
2024-05-15 13:09:06 +0530

    new commit

--- project/src/App.js
+++ project/src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My React Web Page</h1>
+        <h1>We are comming soon</h1>
       </header>
       <main className="App-main">
-        <p>Welcome to my awesome React web page!</p>
-        <p>This is an example of a responsive and attractive web page.</p>
+       <a href='http://legenderfx.com'>legenderfx.com</a>
+       
       </main>
       <footer className="App-footer">
         <p>Created with ❤️ </p>
