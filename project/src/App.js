41114c98c5c8a5a66a65740655e8dd8bcf12f40f
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-05-17 09:02:30 +0000

    n4

--- project/src/App.js
+++ project/src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>We are comming soon</h1>
-        <h1>Hello</h1>
-        <h1>Cricket..</h1>
+        
       </header>
       <main className="App-main">
        <a href='http://legenderfx.com'>legenderfx.com</a>
